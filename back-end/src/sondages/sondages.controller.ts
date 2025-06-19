adb15c5ea249ad5f71e4a0a16bbd93d2078c8add
Furkan Demirbas <m.furkan0508@gmail.com>
2025-06-19 20:14:26 +0200

    Mise à jour du Controlleur

--- back-end/src/sondages/sondages.controller.ts
+++ back-end/src/sondages/sondages.controller.ts
@@ -1,4 +1,43 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Param, Body, ParseIntPipe } from '@nestjs/common';
+import { SondagesService } from './sondages.service';
 
 @Controller('sondages')
-export class SondagesController {}
+export class SondagesController {
+  constructor(private readonly sondagesService: SondagesService) {}
+
+  @Post()
+  async createPoll(@Body() body: {
+    title: string;
+    description?: string;
+    multiple: boolean;
+    options: string[];
+  }) {
+    return this.sondagesService.createPoll(body);
+  }
+
+  @Get()
+  async listPolls() {
+    return this.sondagesService.findAll();
+  }
+
+  @Get(':id/resultats')
+  async getResults(@Param('id', ParseIntPipe) id: number) {
+    return this.sondagesService.getResults(id);
+  }
+
+  @Post(':id/vote')
+  async vote(
+    @Param('id', ParseIntPipe) pollId: number,
+    @Body() body: {
+      voter: string;
+      optionIds: number[];
+    },
+  ) {
+    const voteData = {
+      pollId,
+      voter: body.voter,
+      optionIds: body.optionIds,
+    };
+    return this.sondagesService.vote(voteData);
+  }
+}
